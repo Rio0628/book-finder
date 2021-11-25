c209fd164076993491305acaad177589650fb913
Rio0628 <mariomdomenech@gmail.com>
2021-11-24 23:31:15 -0500

    Created Main Components
    - Created the component directory and created the main components that will be used in the application.
    - Installed the react icons library to populate the nav-bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-
+import { AiOutlineMenu } from 'react-icons/ai'
+import { BiSearchAlt } from 'react-icons/bi'
+ 
 class App extends Component {
   render () {
     
-    Axios.get(`https://www.googleapis.com/books/v1/volumes?q=harry+potter`).then(data => console.log(data) );
+    // Axios.get(`https://www.googleapis.com/books/v1/volumes?q=harry+potter`).then(data => console.log(data) );
 
     return (
       <div className="container">
+        <div className='nav-barCntr'>
+
+        </div>
+
         <h1>Hello</h1>
       </div>
     );
