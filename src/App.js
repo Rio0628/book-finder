58a6f6d5b0f0871e8b5d5582e39ce641b3bd51bb
Rio0628 <mariomdomenech@gmail.com>
2021-11-26 15:24:42 -0500

    Created Comp
    - Created the searchBooksResult Component and the IndBook component that will allow the user to view the result of their search.
    - Gave every even indbook a different background so the user is able to differentiate  the individual books

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Axios from 'axios';
 import { AiOutlineMenu } from 'react-icons/ai'
 import { AiOutlineClose } from 'react-icons/ai'
 import { BiSearchAlt } from 'react-icons/bi'
- 
+import IndSearchBook from './components/IndSearchBook'
+
 class App extends Component {
   render () {
     
@@ -23,16 +24,28 @@ class App extends Component {
           </div>
         </div>
 
-        <h1>Hello</h1>
+        <div className='searchBooksCntr'>
+          <p className='searchRsltsHeading'>Results: Name Of Book</p>
+
+          <div className='booksCntr'>
+            <IndSearchBook />
+            <IndSearchBook />
+            <IndSearchBook />
+            <IndSearchBook />
+            <IndSearchBook />
+            <IndSearchBook />
+          </div>
+        </div>
         
-        <div className='sidebar'>
+        {/* <div className='sidebar'>
           <AiOutlineClose className='sidebarBtnOpen'/>
 
           <div className='categoriesCntr'>
             <p className='category'>Search Book</p>
             <p className='category'>Saved Books</p>
           </div>
-        </div>
+        </div> */}
+      
       </div>
     );
   }
---
+++ src/components/IndSearchBook.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const IndSearchBook = () => {
+    return (
+        <div className='indBookCntr'>
+            <div className='thumbnailBook'></div>
+
+            <div className='mainInfoCntr'>
+                <p className='indBookTitle'>Title of Book</p>
+
+                <div className='authorGenreCntr'>
+                    <p className='indBookAuthor'>Author of Book</p>
+                    <p className='indBookGenre'>Genre Book</p>
+                </div>
+            
+            </div>
+
+         
+        </div>
+    );
+}
+
+export default IndSearchBook;
